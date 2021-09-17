74ffa607eb1b10aa5dadf1a2d7cf9789164658f3
Emmi Siitonen <emmi.siitonen@futurice.com>
2021-09-17 08:22:20 +0300

    Bill input styling

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,28 @@
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="splitter">
+        <h1>splitter</h1>
+      </div>
+      <div className="calculator">
+        <div className="settings">
+          <div className="bill">
+            <label htmlFor="bill">Bill</label>
+            <input
+              type="text"
+              id="bill"
+              className="bill-input"
+              placeholder="0"
+            ></input>
+          </div>
+          <div className="tip-selector">TODO</div>
+          <div className="people-count">TODO</div>
+        </div>
+        <div className="results">TODO</div>
+      </div>
     </div>
   )
 }
