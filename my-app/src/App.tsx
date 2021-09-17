77cfd2ac0e26ae5dc4407909653b18d149a85a37
Emmi Siitonen <emmi.siitonen@futurice.com>
2021-09-17 18:06:21 +0300

    responsivity for left panel

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -14,12 +14,31 @@ function App() {
             <input
               type="text"
               id="bill"
-              className="bill-input"
+              className="input-field--dollar"
               placeholder="0"
             ></input>
           </div>
-          <div className="tip-selector">TODO</div>
-          <div className="people-count">TODO</div>
+          <div className="tip-selector">
+            <label htmlFor="tip-amount">Select Tip %</label>
+            <div className="tip-amount-buttons">
+              {/* TODO: make this dynamic */}
+              <button className="button">5%</button>
+              <button className="button">10%</button>
+              <button className="button">15%</button>
+              <button className="button">25%</button>
+              <button className="button">50%</button>
+              <button className="button button--light">Custom</button>
+            </div>
+          </div>
+          <div className="people-count">
+            <label htmlFor="people-count">Number of People</label>
+            <input
+              type="text"
+              id="people-count"
+              className="input-field--person"
+              placeholder="0"
+            ></input>
+          </div>
         </div>
         <div className="results">TODO</div>
       </div>
